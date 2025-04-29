39bf485800afc7cc2a3572d84d3440521c7edc8c
Default Jarpex <default@jarpex.ru>
2025-04-29 03:10:02 +0300

    fix: CORS update

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ app = FastAPI(title="ToDo App")
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://192.168.1.160:30082",
-    "http://api.todo.jarpex.ru",
+    "http://192.168.1.160:30081",
+    "https://todo.jarpex.com",
 ]
 
 app.add_middleware(
